b5dd4b07666f2d3f0802ab3e94e83e58fdbacfcd
anson <anson.xue@everbridge.com>
2017-07-06 12:53:11 +0800

    fix Jenkinsfile 08

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,26 +1,28 @@
-def getLabelNum(version){
+def getLabelNum(version) {
     def vs = version.split("\\.")
     return "" + vs[0] + vs[1]
 }
 
-def getLabelStr(geo_v, ns_v, ne_v){
+def getLabelStr(geo_v, ns_v, ne_v) {
     return "not geo" + geo_v + "_global" + ns_v + "_instance" + ne_v
 }
 
-def getLabel(label_list, geo_v, ns_v, ne_v){
+def getLabel(label_list, geo_v, ns_v, ne_v) {
     def label_collection = []
-    for (l in label_list){
+    for (l in label_list) {
         def ls = l.split("-")
         def gv = ls[0]
         def nsv = ls[1]
         def nev = ls[2]
-        if(gv != getLabelNum(geo_v) && nsv != getLabelNum(ns_v) && nev != getLabelNum(ne_v)){
+        if (gv == getLabelNum(geo_v) && nsv == getLabelNum(ns_v) && nev == getLabelNum(ne_v)) {
+            continue
+        } else {
             label_collection = label_collection + getLabelStr(gv, nsv, nev)
         }
     }
     def labels = ""
     for (int i = 0; i < label_collection.size(); i++) {
-        if (i != 0){
+        if (i != 0) {
             labels = labels + " and "
         }
         labels = labels + label_collection[i]
@@ -39,8 +41,8 @@ node {
 
     def label_list = []
 
-    
-    stage ('Preparation') {
+
+    stage('Preparation') {
         git credentialsId: 'git-001', url: 'https://github.com/ansonLx/jenkins-jobs.git'
 
         prod_build = sh(script: 'cat ./sh/ne-version | grep "^prod_build=" | sed "s/^prod_build=//"', returnStdout: true).trim()
@@ -62,12 +64,12 @@ node {
         label_list = label_list + (getLabelNum(geo_dev_build) + "-" + getLabelNum(dev_build) + "-" + getLabelNum(prod_build))
 
     }
-    
-    stage ("deploy geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${prod_build}") {
+
+    stage("deploy geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${prod_build}") {
         echo "deploy ${env} geo-version: ${geo_prod_build} ns-version: ${dev_build} ne-version: ${prod_build}"
         sh(script: "./sh/deploy_test.sh ${prod_build} ${dev_build}")
     }
-    stage ("test geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${prod_build}") {
+    stage("test geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${prod_build}") {
         def test_label = getLabel(label_list, geo_prod_build, dev_build, prod_build)
         echo "run test job geo-version: ${geo_prod_build} ns-version: ${dev_build} ne-version: ${prod_build} label: ${test_label}"
 
@@ -78,11 +80,11 @@ node {
                 string(name: 'test_label', value: "${test_label}")]
     }
 
-    stage ("deploy geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${dev_build}") {
+    stage("deploy geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${dev_build}") {
         echo "deploy ${env} geo-version: ${geo_prod_build} ns-version: ${dev_build} ne-version: ${dev_build}"
         sh(script: "./sh/deploy_test.sh ${prod_build} ${dev_build}")
     }
-    stage ("test geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${dev_build}") {
+    stage("test geo_version->${geo_prod_build} ns_version->${dev_build} ne_version->${dev_build}") {
         def test_label = getLabel(label_list, geo_prod_build, dev_build, dev_build)
         echo "run test job geo-version: ${geo_prod_build} ns-version: ${dev_build} ne-version: ${dev_build} label: ${test_label}"
 
